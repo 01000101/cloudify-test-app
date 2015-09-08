471a60b03d88d45bcdff0481424b891bd3e01379
Joshua Cornutt <jcornutt@gmail.com>
2015-09-08 11:02:52 -0400

    Added simple Node.js custom app (self-contained blueprint + app)

---
+++ TestApp1/scripts/nodejs-set-root.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+ctx source instance runtime-properties nodejs_application_path $(ctx target instance runtime_properties nodejs_application_path)
+ctx source instance runtime-properties nodejs_binaries_path $(ctx target instance runtime_properties nodejs_binaries_path)
