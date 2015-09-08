b1f190ee7319e15890d1cabc354940d95e38a6e3
Joshua Cornutt <jcornutt@gmail.com>
2015-09-08 11:41:10 -0400

    Adding monitoring

--- TestApp1/scripts/nodejs-set-root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ctx source instance runtime-properties nodejs_application_path $(ctx target instance runtime_properties nodejs_application_path)
-ctx source instance runtime-properties nodejs_binaries_path $(ctx target instance runtime_properties nodejs_binaries_path)
