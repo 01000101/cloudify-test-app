fd8d7d1a8bb5b75ab2b18cd4d0b38f5ff54f2a44
Joshua Cornutt <jcornutt@gmail.com>
2015-09-16 12:12:55 -0400

    PoC for Oracle RAC key exchange

--- Package1/scripts/poc.sh
+++ Package1/scripts/poc.sh
@@ -4,5 +4,9 @@ set -e
 
 ctx logger info "Printing environment variables..."
 ctx logger info "$(env)"
+ctx logger info "Printing node properties..."
+ctx logger info "$(ctx node properties)"
+ctx logger info "Printing instance properties..."
+ctx logger info "$(ctx instance runtime_properties)"
 ctx logger info "Exiting script"
 
--- Package1/scripts/server-install.sh
+++ Package1/scripts/server-install.sh
@@ -5,4 +5,8 @@ set -e
 mkdir -p /tmp/keys
 ctx logger info "Generating Oracle RAC SSH keys..."
 ssh-keygen -b 2048 -t rsa -N "" -f /tmp/oracle.key
+ctx logger info "Printing node properties..."
+ctx logger info "$(ctx node properties)"
+ctx logger info "Printing instance properties..."
+ctx logger info "$(ctx instance runtime_properties)"
 ctx logger info "Exiting script"
