0cb27cf2a7a8c92fa349677f5dc6f5154ca3c596
Joshua Cornutt <jcornutt@gmail.com>
2015-09-16 15:11:41 -0400

    PoC for Oracle RAC key exchange

--- Package1/scripts/server-install.sh
+++ Package1/scripts/server-install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -e
 
-mkdir -p /tmp/keys
 ctx logger info "Generating Oracle RAC SSH keys..."
 ssh-keygen -b 2048 -t rsa -N "" -f /tmp/oracle.key
 ctx logger info "Printing node properties..."
