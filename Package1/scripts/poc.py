b6b8c125f58cb6093daa3e10316aecda77d974f4
Joshua Cornutt <jcornutt@gmail.com>
2015-09-16 16:30:51 -0400

    PoC for Oracle RAC key exchange

--- Package1/scripts/poc.py
+++ Package1/scripts/poc.py
@@ -3,10 +3,17 @@ from cloudify import ctx
 ctx.logger.info('Initializing plugin script: scripts/PoC.py')
     
 for rel in ctx.instance.relationships:
-    target = rel.target.instance
-    ctx.logger.info('relationship:')
-    ctx.logger.info('  runtime: ' . format(target.runtime_properties))
-    ctx.logger.info('  all: ' . format(target))
+    inst = rel.target.instance
+    node = rel.target.node
+    ctx.logger.info('Relationship:')
+    ctx.logger.info(' Node:')
+    ctx.logger.info('  id: ' . format(node.id))
+    ctx.logger.info('  name: ' . format(node.name))
+    ctx.logger.info('  properties: ' . format(node.properties))
+    ctx.logger.info('  all: ' . format(node))
+    ctx.logger.info(' Instance:')
+    ctx.logger.info('  runtime: ' . format(inst.runtime_properties))
+    ctx.logger.info('  all: ' . format(inst))
     
    
 ctx.logger.info('Node properties: '
