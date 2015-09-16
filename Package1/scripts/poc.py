cc3b6e52ad5a1a035909ee2a690e3d012b0ee592
Joshua Cornutt <jcornutt@gmail.com>
2015-09-16 16:50:30 -0400

    PoC for Oracle RAC key exchange

--- Package1/scripts/poc.py
+++ Package1/scripts/poc.py
@@ -5,14 +5,17 @@ ctx.logger.info('Initializing plugin script: scripts/PoC.py')
 for rel in ctx.instance.relationships:
     inst = rel.target.instance
     node = rel.target.node
-    ctx.logger.info('Relationship:')
+    ctx.logger.info('Relationship ({0}):' . format(rel.type))
     ctx.logger.info(' Node:')
     ctx.logger.info('  id: {0}' . format(node.id))
     ctx.logger.info('  name: {0}' . format(node.name))
     ctx.logger.info('  properties: {0}' . format(node.properties))
     ctx.logger.info('  all: {0}' . format(node))
     ctx.logger.info(' Instance:')
+    ctx.logger.info('  id: {0}' . format(inst.id))
     ctx.logger.info('  runtime: {0}' . format(inst.runtime_properties))
+    ctx.logger.info('  host_ip: {0}' . format(inst.host_ip))
+    ctx.logger.info('  relationships: {0}' . format(inst.relationships))
     ctx.logger.info('  all: {0}' . format(inst))
    
 ctx.logger.info('Node properties: {0}'
