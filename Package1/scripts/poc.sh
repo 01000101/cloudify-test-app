746740d0f688f688216b5d4d67cd3ec2a8eda977
Joshua Cornutt <jcornutt@gmail.com>
2015-09-16 15:32:44 -0400

    PoC for Oracle RAC key exchange

--- Package1/scripts/poc.sh
+++ Package1/scripts/poc.sh
@@ -11,9 +11,7 @@ ctx logger info "$(ctx instance runtime_properties)"
 ctx logger info "Exiting script"
 
 for rel in ctx.instance.relationships:
-    target_instance = rel.target
     ctx logger info "Printing relationship properties..."
     ctx logger info "${rel.target.runtime_properties}"
     ctx logger info "Printing relationship IP..."
     ctx logger info "${rel.target.runtime_properties['ip']}"
-    target_ip = rel.target.runtime_properties['ip']
