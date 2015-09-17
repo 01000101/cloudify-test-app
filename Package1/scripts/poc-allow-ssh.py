ae02ef999eab78b10931c1c2583db1758dc0c7ef
Joshua Cornutt <jcornutt@gmail.com>
2015-09-17 13:17:00 -0400

    Oracle RAC PoC

--- Package1/scripts/poc-allow-ssh.py
+++ Package1/scripts/poc-allow-ssh.py
@@ -34,9 +34,13 @@ if subprocess.call(['mkdir', '-p', os.path.dirname(ORACLE_KEY_PATH)]) != 0:
     raise NonRecoverableError("Error creating path to store Oracle RAC keys")
 
 ctx.logger.info('Generating Oracle RAC keys: {0}' . format(ORACLE_KEY_PATH))
-callout = subprocess.call(['/usr/bin/ssh-keygen', '-t', 'rsa', '-b', '2048', '-N', '""', '-f', ORACLE_KEY_PATH], stderr=subprocess.STDOUT)
-if callout != 0:
-    raise NonRecoverableError("Error generating Oracle RAC keys: {0}" . format(callout))
+
+cmd = '/usr/bin/ssh-keygen -t rsa -b 2048 -N "" -f ' + ORACLE_KEY_PATH
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = p.communicate()
+
+ctx.logger.info('stdout: {0}' . format(stdout))
+ctx.logger.info('stderr: {0}' . format(stderr))
 
 ctx.logger.info('Reading generated Oracle RAC public key')
 with open(ORACLE_KEY_PATH + '.pub', 'r') as f:
