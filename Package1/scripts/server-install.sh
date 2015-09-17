2ecf4e13c1f0904c3586dd927340354db25ede7f
Joshua Cornutt <jcornutt@gmail.com>
2015-09-17 10:17:44 -0400

    Oracle RAC PoC

--- Package1/scripts/server-install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -e
-
-ctx logger info "Generating Oracle RAC SSH keys..."
-ssh-keygen -b 2048 -t rsa -N "" -f /tmp/oracle.key
-ctx logger info "Printing node properties..."
-ctx logger info "$(ctx node properties)"
-ctx logger info "Printing instance properties..."
-ctx logger info "$(ctx instance runtime_properties)"
-ctx logger info "Exiting script"
