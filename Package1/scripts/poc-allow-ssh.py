0d091be786b0248f9cac34c2e53bfd615cc0b611
Joshua Cornutt <jcornutt@gmail.com>
2015-09-17 11:58:57 -0400

    Oracle RAC PoC

--- Package1/scripts/poc-allow-ssh.py
+++ Package1/scripts/poc-allow-ssh.py
@@ -1,20 +1,14 @@
+import os
 from cloudify import ctx
 
 PUB_KEY_FILE = '/tmp/temp.key.pub'
 PUB_KEY_DATA = ''
 SSH_AUTH_FILE = '/home/ubuntu/.ssh/authorized_keys'
+ORACLE_KEY_PATH = ctx.node.properties['oracle_key_path']
 
 ctx.logger.info('Initializing plugin script: scripts/poc-allow-ssh.py')
 
-ctx.logger.info('Node properties: {0}'
-    .format(ctx.node.properties))
- 
-ctx.logger.info('Runtime properties: {0}'
-    .format(ctx.instance.runtime_properties))
-
-ctx.logger.info('Relationships: {0}'
-    .format(ctx.instance.relationships))
-    
+# Install temporary public SSH key into authorized_keys so LAST_NODE can access this system
 ctx.logger.info('Copying temporary SSH key to filesystem')
 ctx.download_resource(ctx.node.properties['tmp_pub_key_path'], PUB_KEY_FILE)
 
@@ -28,7 +22,18 @@ ctx.logger.info('Adding temporary SSH key to authorized_keys list')
 with open(SSH_AUTH_FILE, 'a') as f:
     f.write(PUB_KEY_DATA)
 
-ctx.logger.inf('Restarting the SSH service')
+ctx.logger.info('Reading authorized_keys list')
+with open(SSH_AUTH_FILE, 'r') as f:
+    ctx.logger.info('authorized_keys: {0}' . format(f.read()))
+
+# Generate Oracle RAC keys
+ctx.logger.info('Creating path to store Oracle RAC keys: {0}' . format(os.path.dirname(ORACLE_KEY_PATH)))
+subprocess.call(['mkdir', '-p', os.path.dirname(ORACLE_KEY_PATH)])
+ctx.logger.info('Generating Oracle RAC keys: {0}' . format(ORACLE_KEY_PATH))
+subprocess.call(['ssh-keygen', '-t', 'rsa', '-b', '2048', '-N', '""', '-f', ORACLE_KEY_PATH])
+
+# Enable LAST_NODE to access this system via SSH
+ctx.logger.info('Restarting the SSH service')
 subprocess.call(['sudo', 'ssh', 'restart'])
 
 ctx.logger.info('Plugin script completed')
--- Package1/scripts/poc.py
+++ Package1/scripts/poc.py
@@ -2,10 +2,14 @@ from cloudify import ctx
 
 PRIV_KEY_FILE = '/tmp/temp.key'
 PRIV_KEY_DATA = ''
+ORACLE_KEY_PATH = ctx.node.properties['oracle_key_path']
+NODE1_ORACLE_KEY = '/tmp/node1.rac.key.pub'
+NODE2_ORACLE_KEY = '/tmp/node2.rac.key.pub'
 node_list = []
 
 ctx.logger.info('Initializing plugin script: scripts/poc.py')
 
+ctx.logger.info('Discovering dependent nodes')
 for rel in ctx.instance.relationships:
     if rel.type == 'cloudify.relationships.depends_on':
         node_list.append(rel.target)
@@ -35,5 +39,18 @@ with open(PRIV_KEY_FILE, 'r') as f:
     PRIV_KEY_DATA = f.read()
     
 ctx.logger.info('Temporary SSH key: {0}' . format(PRIV_KEY_DATA))
-    
+
+ctx.logger.info('Copying Oracle RAC public key from Node #1 to {0}' . format(NODE1_ORACLE_KEY))
+subprocess.call(['scp', '-i', PRIV_KEY_FILE, 'ubuntu@' + node_list[0].instance.host_ip + ':' + ORACLE_KEY_PATH, NODE1_ORACLE_KEY])
+ctx.logger.info('Copying Oracle RAC public key from Node #2 to {0}' . format(NODE2_ORACLE_KEY))
+subprocess.call(['scp', '-i', PRIV_KEY_FILE, 'ubuntu@' + node_list[1].instance.host_ip + ':' + ORACLE_KEY_PATH, NODE2_ORACLE_KEY])
+
+ctx.logger.info('Reading Node #1 Oracle key')
+with open(NODE1_ORACLE_KEY, 'r') as f:
+    ctx.logger.info('{0}: {1}' . format(NODE1_ORACLE_KEY, f.read()))
+
+ctx.logger.info('Reading Node #2 Oracle key')
+with open(NODE2_ORACLE_KEY, 'r') as f:
+    ctx.logger.info('{0}: {1}' . format(NODE2_ORACLE_KEY, f.read()))
+
 ctx.logger.info('Plugin script completed')
