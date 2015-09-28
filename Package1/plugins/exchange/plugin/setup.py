45bc152831b625c7a075c63ced5aba8ed4469683
Joshua Cornutt <jcornutt@gmail.com>
2015-09-28 13:49:50 -0400

    plugin dev

---
+++ Package1/plugins/exchange/plugin/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='key-exchange-plugin',
+    description='A Cloudify plugin to exchange information between dynamically instanciated nodes',
+    version='0.1',
+    author='Joshua Cornutt',
+    author_email='josh@gigaspaces.com',
+    install_requires=[
+        'cloudify-plugins-common>=3.2.1'
+    ],
+)
