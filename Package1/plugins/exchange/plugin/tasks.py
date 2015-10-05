6e59279b7d70e2b1fcbeb86dd7be45b46c7ac8ce
Joshua Cornutt <jcornutt@gmail.com>
2015-10-05 15:01:21 -0400

    Converting to use Fabric

--- Package1/plugins/exchange/plugin/tasks.py
+++ Package1/plugins/exchange/plugin/tasks.py
@@ -1,24 +1,14 @@
 import os
 import tempfile
-import uuid
-import json
 import subprocess
 from fabric.api import *
 from cloudify import ctx
-from cloudify.exceptions import NonRecoverableError, RecoverableError
+from cloudify.exceptions import NonRecoverableError
 from cloudify.decorators import operation
 
 # Globals
-XCHG_SSH_AUTH_FILE = ''
-XCHG_KEY_PATH = '/tmp/exchange/xchg.tmp.key'
-XCHG_SSH_USER = ''
-XCHG_NODES = []
 XCHG_RESULT = []
-        
-class ExchangeNode:
-    def __init__(self, ip='', path=''):
-        self.ip = ip
-        self.path = path
+xchgAgents = []
         
 # Find any dependent nodes to retrieve keys from
 def discoverDependents():
@@ -26,106 +16,97 @@ def discoverDependents():
     
     for rel in ctx.instance.relationships:
         if rel.type == 'cloudify.relationships.depends_on':
-            node_list.append(rel.target.instance.host_ip)
+            node_list.append({
+                'ip': rel.target.instance.host_ip,
+                'key_path_remote': rel.target.instance.runtime_properties['key_path'] + '.pub'
+            })
     
     return node_list
 
 # This function retrieves the public key from the remote server,
 # updates XCHG_NODES, and cleans up
 def retrievePublicKey():
-    global XCHG_NODES
+    ctx.logger.info("Executing on {0} as {1}" . format(env.host, env.user))
     
-    ctx.logger.info("Executing on {0} as {1}" . format(
-        env.host,
-        env.user
-    ))
+    # Find ourselves...
+    for idx, xchgAgent in xchgAgents:
+        if xchgAgent['ip'] == env.host:
+            # Generate temporary file
+            fd, sPath = tempfile.mkstemp()
+            with os.fdopen(fd, 'w+') as temp_file:
+                # Copy the remote public key to the temporary file
+                ctx.logger.info('fabric.operations.get({0}, {1})' . format(xchgAgent['key_path_remote'], sPath))
+                get(xchgAgent['key_path_remote'], temp_file)
+                
+                # Log where we stashed the public key
+                xchgAgents[idx]['key_path_local'] = sPath
+            
+            return
 
-    # Generate temporary file
-    fd, sPath = tempfile.mkstemp()
-    temp_file = os.fdopen(fd, 'w+')
-    
-    # Copy the remote public key to the temporary file
-    ctx.logger.info('fabric.operations.get({0}, {1})' . format(XCHG_KEY_PATH + '.pub', sPath))
-    res = get(XCHG_KEY_PATH + '.pub', temp_file)
-    ctx.logger.info(' -> {0}' . format(res))
-    
-    # Log where we stashed the public key
-    XCHG_NODES.append(ExchangeNode(env.host, sPath))
-    
-    # Close the temporary file
-    temp_file.close()
-
+# This function faciliates the exchange of keys between servers
 def exchangePublicKeys():
     global XCHG_RESULT
+    global xchgAgents
     exchanges = []
-    nodeTo = None;
     
-    ctx.logger.info("Executing on {0} as {1}" . format(
-        env.host,
-        env.user
-    ))
+    ctx.logger.info("Executing on {0} as {1}" . format(env.host, env.user))
     
-    for node in XCHG_NODES:
-        if node.ip == env.host:
-            nodeTo = node
-            break
+    # Find ourselves...
+    for idxTo, xchgAgentTo in xchgAgents:
+        if xchgAgent['ip'] == env.host:
+            # Iterate through all nodes (except for ourself)
+            for xchgAgentFrom in xchgAgents:
+                if xchgAgentFrom['ip'] != xchgAgentTo['ip']:
+                    ctx.logger.info('Moving public key from {0} to {1}' . format(
+                        xchgAgentFrom['ip'],
+                        xchgAgentTo['ip']
+                    ))
+                
+                    # Copy the public key from local to a remote node
+                    res = put(nxchgAgentFrom['key_path_local'], '/tmp/')
+                    
+                    ctx.logger.info(' -> file moved to {0}:{1}' . format(
+                        xchgAgentTo['ip'],
+                        res[0]
+                    ))
+                    
+                    # Log the exchange (from IP, public key path)
+                    exchanges.append({
+                        'from': xchgAgentFrom['ip'],
+                        'key_path': res[0]
+                    })
     
-    for nodeFrom in XCHG_NODES:
-        if nodeFrom.ip != nodeTo.ip:
-            ctx.logger.info('Moving public key from {0} to {1}' . format(
-                nodeFrom.ip,
-                nodeTo.ip
-            ))
-            res = put(nodeFrom.path, '/tmp/')
-            ctx.logger.info(' -> file moved to {0}:{1}' . format(
-                nodeTo.ip,
-                res
-            ))
-            
-            # Log every exchange (to/from IP, public key path)
-            exchanges.append({
-                'from': nodeFrom.ip,
-                'key_path': res[0]
+            # Log the entire exchange for this node to the outputs list
+            XCHG_RESULT.append({
+                'to': xchgAgentTo['ip'],
+                'exchanges': exchanges
             })
-    
-    # Log the entire exchange for this node
-    XCHG_RESULT.append({
-        'to': nodeTo.ip,
-        'exchanges': exchanges
-    })
 
 
 @operation
 def configure(**kwargs):
-    global XCHG_SSH_USER
-    global XCHG_SSH_AUTH_FILE
     global XCHG_NODES
-    
-    XCHG_SSH_USER = ctx.node.properties['facilitator_agent_ssh_user']
-    XCHG_SSH_AUTH_FILE = ctx.node.properties['facilitator_agent_authorized_keys_path']
-    XCHG_PRIVATE_KEY_PATH = ctx.node.properties['facilitator_private_key_path']
+    global xchgAgents
+    env.hosts = []
     
     ctx.logger.info('Discovering dependent nodes')
-    nodeIpList = discoverDependents()
+    xchgAgents = discoverDependents()
     
     # If less than 2 nodes were discovered, that's a problem...
-    ctx.logger.info('{0} nodes discovered' . format(len(nodeIpList)))
-    if len(nodeIpList) < 2:
+    ctx.logger.info('{0} nodes discovered' . format(len(xchgAgents)))
+    if len(xchgAgents) < 2:
         raise NonRecoverableError("Exchange plugin requires at least 2 dependent nodes")
     
-    # Output each node IP
-    for nodeIp in nodeIpList:
-        ctx.logger.info(' IP: {0}' . format(nodeIp))
+    # Add each found node IP to the Fabric hosts list
+    for xchgAgent in xchgAgents:
+        ctx.logger.info(' IP: {0}' . format(xchgAgent['ip']))
+        env.hosts.append(xchgAgent['ip'])
     
-    # Give Fabric our remote hosts list & settings
-    env.hosts = nodeIpList
+    # Give Fabric our connection settings
     env.user = ctx.bootstrap_context.cloudify_agent.user
     env.key_filename = ctx.bootstrap_context.cloudify_agent.agent_key_path
     env.password = None
     env.disable_known_hosts = True
-    
-    ctx.logger.info('Manager user: {0}' . format(env.user))
-    ctx.logger.info('Manager key: {0}' . format(env.key_filename))
     
     # Retrieve public keys from each of the nodes
     execute(retrievePublicKey)
@@ -136,26 +117,26 @@ def configure(**kwargs):
     # Create our output
     ctx.logger.info('Exchanges: {0}' . format(XCHG_RESULT))
     ctx.instance.runtime_properties['exchange_result'] = XCHG_RESULT
-
-    ctx.logger.info('Plugin script completed')
     
 @operation
 def install_linux_agent(**kwargs):
-    # Generate Oracle RAC keys
-    ctx.logger.info('Creating path to store keys: {0}' . format(os.path.dirname(XCHG_KEY_PATH)))
-    if not os.path.exists(os.path.dirname(XCHG_KEY_PATH)):
-        os.makedirs(os.path.dirname(XCHG_KEY_PATH))
+    # Create a temporary file to store our key in
+    fd, sPath = tempfile.mkstemp()
+    # Close the file we just made
+    os.close(fd)
     
-    if not os.path.exists(os.path.dirname(XCHG_KEY_PATH)):
-        ctx.logger.info('Creating path failed')
-        
-    ctx.logger.info('Generating keys: {0}' . format(XCHG_KEY_PATH))
-    if subprocess.call('ssh-keygen -t rsa -b 2048 -N "" -f ' + XCHG_KEY_PATH, shell=True) != 0:
-        if not os.path.exists(XCHG_KEY_PATH):
+    # Generate the SSH keys
+    ctx.logger.info('Generating keys: {0}[.pub]' . format(sPath))
+    if subprocess.call('ssh-keygen -t rsa -b 2048 -N "" -f ' + sPath, shell=True) != 0:
+        if (not os.path.exists(sPath)) or (not os.path.exists(sPath + '.pub')):
             raise NonRecoverableError("Error generating keys")
-    
+
+    # Confirm the keys are OK
     ctx.logger.info('Reading generated public key')
-    with open(XCHG_KEY_PATH + '.pub', 'r') as f:
-        ctx.logger.info('{0}: {1}' . format(XCHG_KEY_PATH + '.pub', f.read()))
+    with open(sPath + '.pub', 'r') as f:
+        ctx.logger.info('{0}: {1}' . format(sPath + '.pub', f.read()))
+    
+    # Store the location of the key to our runtime properties
+    ctx.instance.runtime_properties['key_path'] = sPath
     
     ctx.logger.info('Plugin script completed')
