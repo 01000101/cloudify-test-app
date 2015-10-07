82aa92c96d53f5abbf654ae59d312236638e28e0
Joshua Cornutt <jcornutt@gmail.com>
2015-10-07 17:13:21 -0400

    Reboot issue

--- reboot/scripts/prereboot.py
+++ reboot/scripts/prereboot.py
@@ -4,6 +4,6 @@ from cloudify import ctx
 nSec = 15
 
 ctx.logger.info('Rebooting the host VM in {0} seconds...' . format(nSec))
-proc = subprocess.Popen(['sudo', 'shutdown', '-r', '-t {0}' . format(nSec)], stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen(['shutdown', '-r', nSec], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 ctx.logger.info('Reboot request output: {0} ({1})' . format(out, err))
